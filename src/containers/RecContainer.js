9fa4c4dd7c5a62c6a1d4232d5c81dfd1f13d2b23
github username <github email address>
2019-11-29 08:58:21 -0500

    updated modal details

--- src/containers/RecContainer.js
+++ src/containers/RecContainer.js
@@ -46,7 +46,7 @@ export default class RecContainer extends Component {
   };
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <>
         <div className="rec-bg">
